effa7b04e0e851a5b3bddb66087c5903e6a99363
gaobaotai <baotaig@shzexin.com>
2022-11-09 14:12:25 +0800

    22-11-09

---
+++ 88.合并两个有序数组.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=golang
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+	sorted  := make([]int, 0,m+n)
+	p1,p2 := 0 ,0
+
+	for {
+		if p1 == m{
+			sorted = append(sorted,nums2[p2:]...)
+			break
+		}
+		if p2 == n{
+			sorted = append(sorted,nums1[p1:]...)
+			break
+		}
+
+		if nums1[p1]>nums2[p2]{
+			sorted = append(sorted,nums2[p2])
+			p2++
+		}else{
+			sorted = append(sorted,nums1[p1])
+			p1++
+		}
+	}
+	copy(nums1,sorted)
+}
+// @lc code=end
+
